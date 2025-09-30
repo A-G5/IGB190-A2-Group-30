f0295dba569ce34a6bea4e95e5ba6a8ebd79784e
alecomac <alejammcd@gmail.com>
2025-09-30 14:20:14 +1000

    Attempt to add in hitstun
    TRYING TO ADD HITSTUN

---
+++ Assets/Core/Scripts/MonsterEndStun.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterEndStun : MonoBehaviour
+{
+    public GameObject monster;
+    private void EndStun()
+    {
+        if (monster != null)
+        {
+            monster.GetComponent<Monster>().isStunned = false;
+            Debug.Log("Stop stunned!");
+        }
+        
+    }
+}
