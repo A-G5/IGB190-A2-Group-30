ffc95acbd2412ff1d5dd10159707af3a3183b58c
alecomac <alejammcd@gmail.com>
2025-10-25 22:40:02 +1000

    Monster Ghost stun immunity and haunting

--- Assets/Core/Scripts/MonsterEndStun.cs
+++ Assets/Core/Scripts/MonsterEndStun.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class MonsterEndStun : MonoBehaviour
 {
     public GameObject monster;
+    public bool stunImmune;
+
+    private void Update()
+    {
+        if (stunImmune == true)
+        {
+            monster.GetComponent<Monster>().isStunned = false;
+        }
+    }
+
     private void EndStun()
     {
         if (monster != null)
         {
             monster.GetComponent<Monster>().isStunned = false;
         }
-        
     }
 }
